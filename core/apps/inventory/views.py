e0ce2ff6bf27e1b09e230dfed56e8efbb1990b43
rachitpoudel101 <rachit300poudel@gmail.com>
2025-09-07 17:25:45 +0545

    Make file and update in requirment file

--- core/apps/inventory/views.py
+++ core/apps/inventory/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError
 
 from core.apps.inventory.models import (
     Category,
